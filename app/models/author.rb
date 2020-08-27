baad52c288d3cc9ba17c2736929dd2cda913aa7e
github username <github email address>
2020-08-27 01:08:27 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,4 @@
 class Author < ActiveRecord::Base
-    validates :name, presence: true
+    validates :name, presence: true, uniqueness: true
     validates :phone_number, length: { is: 10 }
-    validates :name, uniqueness: true
 end
